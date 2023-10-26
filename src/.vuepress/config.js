0568d62167682297f5689e638bb5369f9daba678
Arie Timmerman <arietimmerman@gmail.com>
2023-10-26 22:13:35 +0200

    updates

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -27,11 +27,7 @@ module.exports = {
 							{ text: 'Azure AD connector for SailPoint', link: '/software/sailpoint/' },
 						]
 					},
-					{
-						text: 'Workshops', items: [
-							{ text: 'Identity Governance and Azure AD', link: '/workshops/azuread/' },
-						]
-					},
+					
 					/*
 					{
 						text: 'Services', items: [
